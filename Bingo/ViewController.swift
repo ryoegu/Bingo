1acc825685e5cf2ce9a2097d683530b0624a23d8
Ryo Eguchi <eguchi@lifeistech.co.jp>
2015-09-13 08:29:34 +0900

    Initial Commit

---
+++ Bingo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bingo
+//
+//  Created by Ryo Eguchi on 2015/09/13.
+//  Copyright (c) 2015年 Ryo Eguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
